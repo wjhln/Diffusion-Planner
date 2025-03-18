fix bug for normalization json, drop ego agent past

diffusion_planner/data_process/data_processor.py
@@ -69,7 +69,7 @@ class DataProcessor(object):
 
         
         data = {"neighbor_agents_past": neighbor_agents_past[:, -21:],
-                "ego_current_state": np.array([0., 0., 1. ,0.], dtype=np.float32), # ego centric x, y, cos, sin
+                "ego_current_state": np.array([0., 0., 1. ,0., 0., 0., 0., 0., 0., 0.], dtype=np.float32), # ego centric x, y, cos, sin, vx, vy, ax, ay, steering angle, yaw rate, we only use x, y, cos, sin during inference
                 "static_objects": static_objects}
         data.update(vector_map)
         data = convert_to_model_inputs(data, device)
@@ -147,10 +147,10 @@ class DataProcessor(object):
             '''
             ego current
             '''
-            ego_agent_past, ego_current_state = calculate_additional_ego_states(ego_agent_past, time_stamps_past)
+            ego_current_state = calculate_additional_ego_states(ego_agent_past, time_stamps_past)
 
             # gather data
-            data = {"map_name": map_name, "token": token, "ego_agent_past": ego_agent_past, "ego_current_state": ego_current_state, "ego_agent_future": ego_agent_future,
+            data = {"map_name": map_name, "token": token, "ego_current_state": ego_current_state, "ego_agent_future": ego_agent_future,
                     "neighbor_agents_past": neighbor_agents_past, "neighbor_agents_future": neighbor_agents_future, "static_objects": static_objects}
             data.update(vector_map)
 

diffusion_planner/data_process/ego_process.py
@@ -87,13 +87,6 @@ def calculate_additional_ego_states(ego_agent_past, time_stamp):
         steering_angle = np.clip(steering_angle, -2 / 3 * np.pi, 2 / 3 * np.pi)
         yaw_rate = np.clip(yaw_rate, -0.95, 0.95)
 
-    past = np.zeros((ego_agent_past.shape[0], ego_agent_past.shape[1] + 1), dtype=np.float32)
-
-    past[:, :2] = ego_agent_past[:, :2]
-    past[:, 2] = np.cos(ego_agent_past[:, 2])
-    past[:, 3] = np.sin(ego_agent_past[:, 2])
-    past[:, 4:] = ego_agent_past[:, 3:]
-
     current = np.zeros((ego_agent_past.shape[1] + 3), dtype=np.float32)
     current[:2] = current_state[:2]
     current[2] = np.cos(current_state[2])
@@ -102,4 +95,4 @@ def calculate_additional_ego_states(ego_agent_past, time_stamp):
     current[8] = steering_angle
     current[9] = yaw_rate
 
-    return past, current
+    return current

diffusion_planner/model/module/decoder.py
@@ -51,7 +51,7 @@ class Decoder(nn.Module):
             inputs: Dict
                 {
                     ...
-                    "ego_agent_past": past and current ego states,            
+                    "ego_current_state": current ego states,            
                     "neighbor_agent_past": past and current neighbor states,  
 
                     [training-only] "sampled_trajectories": sampled current-future ego & neighbor states,        [B, P, 1 + V_future, 4]